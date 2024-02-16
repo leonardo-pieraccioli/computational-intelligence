9a5befeceb4a765a3815086b87d7c19e9e155591
Leonardo Pieraccioli <leonardo.pieraccioli@gmail.com>
2024-02-16 11:55:11 +0100

    more tests

--- Quixo/main.py
+++ Quixo/main.py
@@ -11,30 +11,58 @@ win = [0, 0]
 # g = Game()
 # winner = g.play(MinmaxPlayer(0), HumanPlayer(), True)
 
-win1 = 0
+print('Win counters take into account the second player (1)')
+
+win = 0
 with tqdm.tqdm(total=NUM_GAMES) as pbar:
     for _ in range(NUM_GAMES):
         g = Game()
-        win1 += g.play(MinmaxPlayer(0, 1), RandomPlayer())
+        win += g.play(MinmaxPlayer(0, 1), RandomPlayer())
         pbar.update(1)
+print(win, ' games won by Random against Minmax (1)')
 
-
-print(win1)
-
-win2 = 0
+win = 0
 with tqdm.tqdm(total=NUM_GAMES) as pbar:
     for _ in range(NUM_GAMES):
         g = Game()
-        win2 += g.play(MinmaxPlayer(1, 2), MinmaxPlayer(1, 1))
+        win += g.play(RandomPlayer(), MinmaxPlayer(1, 1))
         pbar.update(1)
+        
+print(win, ' games won by Minmax (1) against Random')
 
-print(win2)
-
-win3 = 0
+win = 0
 with tqdm.tqdm(total=NUM_GAMES) as pbar:
     for _ in range(NUM_GAMES):
         g = Game()
-        win3 += g.play(MinmaxPlayer(1, 1), MinmaxPlayer(1, 2))
+        win += g.play(MinmaxPlayer(0, 2), RandomPlayer())
+        pbar.update(1)
+        
+print(win, ' games won by Random against Minmax (2)')
+
+win = 0
+with tqdm.tqdm(total=NUM_GAMES) as pbar:
+    for _ in range(NUM_GAMES):
+        g = Game()
+        win += g.play(RandomPlayer(), MinmaxPlayer(1, 2))
         pbar.update(1)
 
-print(win3)
+print(win, ' games won by Minmax (2) against Random')
+
+win = 0
+with tqdm.tqdm(total=NUM_GAMES) as pbar:
+    for _ in range(NUM_GAMES):
+        g = Game()
+        win += g.play(MinmaxPlayer(0, 2), MinmaxPlayer(1, 1))
+        pbar.update(1)
+
+print(win, ' games won by Minmax (2) against Minmax (1)')
+
+win = 0
+with tqdm.tqdm(total=NUM_GAMES) as pbar:
+    for _ in range(NUM_GAMES):
+        g = Game()
+        win += g.play(MinmaxPlayer(0, 1), MinmaxPlayer(1, 2))
+        pbar.update(1)
+
+print(win, ' games won by Minmax (1) against Minmax (2)')
+
