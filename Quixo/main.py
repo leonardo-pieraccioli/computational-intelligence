cffab32690c83974e5c092d0b716b1ed981aae73
Leonardo Pieraccioli <leonardo.pieraccioli@gmail.com>
2024-02-17 17:28:57 +0100

    select number of games from terminal

--- Quixo/main.py
+++ Quixo/main.py
@@ -5,7 +5,8 @@ from human_player import HumanPlayer
 import time as t
 import tqdm
 
-NUM_GAMES = 20
+print('List of games:\n\t1. Minmax (1) vs Random\n\t2. Random vs Minmax (1)\n\t3. Minmax (2) vs Random\n\t4. Random vs Minmax (2)\n\t5. Minmax (2) vs Minmax (1)\n\t6. Minmax (1) vs Minmax (2)\n\t7. Minmax (1) vs Minmax (1)\n\t8. Minmax (2) vs Minmax (2)\n')
+NUM_GAMES = int(input('How many games do you want to play? '))
 win = [0, 0]
 
 # g = Game()
@@ -13,6 +14,7 @@ win = [0, 0]
 
 print('Win counters take into account the second player (1)')
 
+print('Minmax (1) vs Random')
 win = 0
 with tqdm.tqdm(total=NUM_GAMES) as pbar:
     for _ in range(NUM_GAMES):
@@ -21,6 +23,7 @@ with tqdm.tqdm(total=NUM_GAMES) as pbar:
         pbar.update(1)
 print(win, ' games won by Random against Minmax (1)')
 
+print('Random vs Minmax (1)')
 win = 0
 with tqdm.tqdm(total=NUM_GAMES) as pbar:
     for _ in range(NUM_GAMES):
@@ -30,6 +33,7 @@ with tqdm.tqdm(total=NUM_GAMES) as pbar:
         
 print(win, ' games won by Minmax (1) against Random')
 
+print('Minmax (2) vs Random')
 win = 0
 with tqdm.tqdm(total=NUM_GAMES) as pbar:
     for _ in range(NUM_GAMES):
@@ -39,6 +43,7 @@ with tqdm.tqdm(total=NUM_GAMES) as pbar:
         
 print(win, ' games won by Random against Minmax (2)')
 
+print('Random vs Minmax (2)')
 win = 0
 with tqdm.tqdm(total=NUM_GAMES) as pbar:
     for _ in range(NUM_GAMES):
@@ -48,6 +53,7 @@ with tqdm.tqdm(total=NUM_GAMES) as pbar:
 
 print(win, ' games won by Minmax (2) against Random')
 
+print('Minmax (2) vs Minmax (1)')
 win = 0
 with tqdm.tqdm(total=NUM_GAMES) as pbar:
     for _ in range(NUM_GAMES):
@@ -57,11 +63,32 @@ with tqdm.tqdm(total=NUM_GAMES) as pbar:
 
 print(win, ' games won by Minmax (2) against Minmax (1)')
 
+print('Minmax (1) vs Minmax (2)')
 win = 0
 with tqdm.tqdm(total=NUM_GAMES) as pbar:
     for _ in range(NUM_GAMES):
         g = Game()
         win += g.play(MinmaxPlayer(0, 1), MinmaxPlayer(1, 2))
+        pbar.update(1)
+
+print(win, ' games won by Minmax (1) against Minmax (2)')
+
+print('Minmax (1) vs Minmax (1)')
+win = 0
+with tqdm.tqdm(total=NUM_GAMES) as pbar:
+    for _ in range(NUM_GAMES):
+        g = Game()
+        win += g.play(MinmaxPlayer(0, 1), MinmaxPlayer(1, 1))
+        pbar.update(1)
+
+print(win, ' games won by Minmax (1) against Minmax (2)')
+
+print('Minmax (2) vs Minmax (2)')
+win = 0
+with tqdm.tqdm(total=NUM_GAMES) as pbar:
+    for _ in range(NUM_GAMES):
+        g = Game()
+        win += g.play(MinmaxPlayer(0, 2), MinmaxPlayer(1, 2))
         pbar.update(1)
 
 print(win, ' games won by Minmax (1) against Minmax (2)')
